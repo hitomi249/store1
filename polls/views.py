ca00270b0b87ff95620fd067ab8ece186e5f532b
hitomi249 <coloful.i.o.i@gmai.com>
2024-04-14 11:55:41 +0900

    views.pyでindexビューを更新

--- polls/views.py
+++ polls/views.py
@@ -2,16 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Question
 
-
-
-
+#views.pyでindexビューを更新
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output =", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
