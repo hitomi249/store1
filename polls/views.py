e899700d199b7f475b11c5714407c80468abb352
hitomi249 <coloful.i.o.i@gmai.com>
2024-04-14 15:48:44 +0900

    vote関数の実装

--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,20 @@
-from django.http import Http404
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-
-# Create your views here.
-from django.http import HttpResponse
+from django.urls import reverse
 
 
-from .models import Question
+from .models import Choice, Question
 
-#views.pyでindexビューを更新
+
+# views.pyでindexビューを更新
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {
         "latest_question_list": latest_question_list,
     }
     return render(request, "polls/index.html", context)
+
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -26,4 +27,19 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
