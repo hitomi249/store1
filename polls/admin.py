655de0b47e0ffc10116311e9c2c794c7ba7f4f96
hitomi249 <coloful.i.o.i@gmai.com>
2024-04-18 21:49:08 +0900

    questionに情報を追加

--- polls/admin.py
+++ polls/admin.py
@@ -16,6 +16,7 @@ class QuestionAdmin(admin.ModelAdmin):
         ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
     inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
 
 
 admin.site.register(Question, QuestionAdmin)
